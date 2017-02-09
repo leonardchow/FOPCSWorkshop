06073cfc1d8006f71b070d354ba9cd9173d32825
leonardchow <leonardchowpk@gmail.com>
2017-02-09 09:55:48 +0800

    Added Completed Quiz Day 5
    This includes the amendment of  a for loop that converts char to int.

--- FOPCSWorkshop/Quizzes.cs
+++ FOPCSWorkshop/Quizzes.cs
@@ -6,7 +6,42 @@ namespace FOPCSWorkshop
     {
         public static void Main(string[] args)
         {
-            QuizDay4();
+            QuizDay5();
+
+            return;
+        }
+
+        public static void QuizDay5()
+        {
+            string input = "";
+            int[] array = new int[5];
+            Console.Write("Please enter matriculation number: ");
+            input = Console.ReadLine();
+
+            if (input.Length != 7)
+            {
+                Console.WriteLine("Invalid");
+                return;
+            }
+
+            input = input.ToUpper();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = input[i + 1] - '0';
+            }
+
+            int sum = array[0] * 6 + array[1] * 5 + array[2] * 4 + array[3] * 3 + array[4] * 2;
+            Console.WriteLine(sum);
+            int rem = sum % 5;
+
+            if ((input[6] - 'O') == rem)
+            {
+                Console.WriteLine("Valid");
+            } else
+            {
+                Console.WriteLine("Invalid");
+            }
 
             return;
         }
